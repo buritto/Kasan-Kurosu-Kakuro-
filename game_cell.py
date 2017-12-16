cc9a0dfbad53992602d57c1e4137e91a281415a2
buritto <ybiz177@gmail.com>
2017-12-16 16:36:52 +0500

    added check for rules cells

--- game_cell.py
+++ game_cell.py
@@ -1,10 +1,15 @@
+import cell_type
+
+
 class Cell:
 
-    def __init__(self, type_cell, value=0, row_rule=0, column_rule=0):
+    def __init__(self, type_cell, value=0, row_rule=-1, column_rule=-1, length_row=0, length_column=0):
         self._type_cell = type_cell
         self._value = value
         self._row_rule = row_rule
         self._column_rule = column_rule
+        self._length_row = length_row
+        self._length_column = length_column
 
     def get_value(self):
         return self._value
@@ -14,3 +19,12 @@ class Cell:
 
     def get_rules(self):
         return [self._row_rule, self._column_rule]
+
+    def is_rules(self):
+        return self._type_cell == cell_type.CellType.RULES
+
+    def get_length_row(self):
+        return self._length_row
+
+    def gey_length_column(self):
+        return self._length_column
--- game_field.py
+++ game_field.py
@@ -1,4 +1,4 @@
-from cell import Cell
+from game_cell import Cell
 from cell_type import CellType
 
 
@@ -7,7 +7,34 @@ class GameField:
         self.width = width
         self.height = height
         self.filed = []
-        for x in range(0, width):
+        for x in range(0, height):
             self.filed[x] = []
-            for y in range(0, height):
+            for y in range(0, width):
                 self.filed[x][y] = Cell(CellType.NO_ACTIVE)
+
+    def init_cell(self, x, y, cell):
+        self.filed[x][y] = cell
+
+    def check_correct_field(self):
+        for x in range(0, self.height):
+            for y in range(0, self.width):
+                if self.filed[x][y].is_rules():
+                    if not self.correct_row(x, y + 1, self.filed[x][y].get_length_row()):
+                        return False
+                    if not self.correct_column(x + 1, y, self.filed[x][y].get_length_column()):
+                        return False
+        return True
+
+    def correct_row(self, pos_x, pos_y, length_row):
+        for y in range(0, length_row):
+            if self.filed[pos_x][pos_y + y].get_type() != CellType.PLAY:
+                return False
+        return True
+
+    def correct_column(self, pos_x, pos_y, length_column):
+        for x in range(0, length_column):
+            if self.filed[pos_x + x][pos_y].get_type() != CellType.PLAY:
+                return False
+        return True
+
+
