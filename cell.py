ad860cf114ef5801a9281eec462b15f41c7b700f
buritto <ybiz177@gmail.com>
2017-12-16 15:09:34 +0500

    added new method in Cell

--- cell.py
+++ cell.py
@@ -1,7 +1,16 @@
 class Cell:
 
     def __init__(self, type_cell, value=0, row_rule=0, column_rule=0):
-        self.type_cell = type_cell
-        self.value = value
-        self.row_rule = row_rule
-        self.column_rule = column_rule
+        self._type_cell = type_cell
+        self._value = value
+        self._row_rule = row_rule
+        self._column_rule = column_rule
+
+    def get_value(self):
+        return self._value
+
+    def get_type(self):
+        return self._type_cell
+
+    def get_rules(self):
+        return [self._row_rule, self._column_rule]
--- game_field.py
+++ game_field.py
@@ -1,4 +1,6 @@
 from cell import Cell
+from cell_type import CellType
+
 
 class GameField:
     def __init__(self, width, height):
@@ -8,4 +10,4 @@ class GameField:
         for x in range(0, width):
             self.filed[x] = []
             for y in range(0, height):
-                self.filed[x][y] = cell.Cell()
+                self.filed[x][y] = Cell(CellType.NO_ACTIVE)
