f356df9d68d283e5166eeaaa000ecb0464ad6a54
buritto <ybiz177@gmail.com>
2017-12-16 15:55:02 +0500

    added correct word testt

--- test/test_cell.py
+++ test/test_cell.py
@@ -1,22 +1,30 @@
 import unittest
-from cell import Cell
+import sys
+print(sys.path)
+sys.path.append('C:\\Programm\\KasanKurosu\\Kasan-Kurosu-Kakuro-')
+
 from cell_type import CellType
+from game_cell import Cell
 
 
-class CellTest(unittest):
+class CellTest(unittest.TestCase):
 
     def test_get_value(self):
         value = 10
-        cell = Cell(CellType.PLAY, value)
-        self.assertEqual(value, cell.get_value())
+        c = Cell(CellType.PLAY, value)
+        self.assertEqual(value, c.get_value())
 
     def test_get_type(self):
         type = CellType.NO_ACTIVE
-        cell = Cell(type)
-        self.assertEquale(type, cell.get_type())
+        c = Cell(type)
+        self.assertEqual(type, c.get_type())
 
     def test_get_rule(self):
         rule_row = 10
         rule_column = 10
-        cell = Cell(CellType.RULES, column_rule=rule_column, row_rule=rule_row)
-        self.assertEquale([rule_row, rule_column], cell.get_rules())
+        c = Cell(CellType.RULES, column_rule=rule_column, row_rule=rule_row)
+        self.assertEqual([rule_row, rule_column], c.get_rules())
+
+
+if __name__ == '__main__':
+    unittest.main()
